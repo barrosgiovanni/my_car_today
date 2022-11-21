b6e67946dd750628611f0c89e9b28fb0e4f9424a
Giovanni Barros <barrosgiovanni@icloud.com>
2022-11-21 19:38:43 -0300

    devise user, car model, car  controller, booking model and associations are done! Leeeets go for day2!

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_215529) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_21_221545) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "pick_up_date"
+    t.date "drop_of_date"
+    t.string "location"
+    t.string "status"
+    t.bigint "user_id", null: false
+    t.bigint "car_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_bookings_on_car_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "cars", force: :cascade do |t|
     t.integer "price"
@@ -41,5 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_215529) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "cars"
+  add_foreign_key "bookings", "users"
   add_foreign_key "cars", "users"
 end
