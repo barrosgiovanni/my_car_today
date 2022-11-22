1b0190c7700a1093f15236c8ca62bd201a23f30b
julia-finamore <juliamm_finamore@hotmail.com>
2022-11-22 17:04:13 -0300

    controller show ok, route show ok, show html e css ok

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,10 @@
 class Car < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  validates :model, presence: true
+  validates :brand, presence: true
+  validates :year, presence: true
+  validates :color, presence: true
+  validates :category, presence: true
+  validates :price, presence: true
 end
